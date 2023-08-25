6f8eb535b3273c73f8193268f686d2da0ddd1716
Jsyrett977 <Jsyrett977@gmail.com>
2023-08-25 10:42:20 -0600

    check for win completed

--- src/components/Tic.js
+++ src/components/Tic.js
@@ -15,20 +15,63 @@ const Tic = () => {
     const [playerOne, setPlayerOne] = useState('X')
     const [playerTwo, setPlayerTwo] = useState('O')
     const [currentPlayer, setCurrentPlayer] = useState(playerOne)
-    const checkWin = () => {
-        let count = 0  
+    const checkHorizontalWin = () => { 
         for(let i = 0; i < 3; i++){
-            count = 0
+            let xCount = 0
+            let oCount = 0
             for(let j = 0; j < 3; j++){
                 if(board[i][j] === 'X'){
-                    count+= 1
-                    count === 3 ? console.log('Player One Wins') : null
+                    xCount+= 1
+                    xCount === 3 ? console.log('Player One Wins') : null
                 }else if(board[i][j] === 'O'){
-
+                    oCount+= 1
+                    oCount === 3 ? console.log('Player Two Wins') : null
                 }
             }
         }
     }
+    const checkVerticalWin = () => {
+        for(let i = 0; i < 3; i++){
+            let xCount = 0
+            let oCount = 0
+            for(let j = 0; j < 3; j++){
+                if(board[j][i] === 'X'){
+                    xCount+= 1
+                    xCount === 3 ? console.log('Player One Wins') : null
+                }else if(board[j][i] === 'O'){
+                    oCount+= 1
+                    oCount === 3 ? console.log('Player Two Wins') : null
+                }
+            }
+        }
+    }
+    const checkDiagonalWin = () => {
+        let xCount = 0
+        let oCount = 0
+        let xxCount = 0
+        let ooCount = 0
+        for(let i = 0; i < 3; i++){
+            if(board[i][i] === 'X'){
+                xCount+= 1
+                xCount === 3 ? console.log('Player One Wins') : null
+            }else if(board[i][i] === 'O'){
+                oCount+= 1
+                oCount === 3 ? console.log('Player Two Wins') : null
+            }
+        }
+        for(let i = 0; i < 3; i++){
+            let j = 2 - i
+            if(board[i][j] === 'X'){
+                    xxCount+= 1
+                    xxCount === 3 ? console.log('Player One Wins') : null
+            }else if(board[i][j] === 'O'){
+                ooCount+= 1
+                ooCount === 3 ? console.log('Player Two Wins') : null
+            }
+                
+            }
+        }
+    
     const handleClick = () => {
         const newBoard = [...board]
         const x = Number(event.target.id[0])
@@ -43,8 +86,9 @@ const Tic = () => {
         setBoard(newBoard)
     }
     useEffect(() => {
-        checkWin()
-        console.log('no')
+        checkHorizontalWin()
+        checkVerticalWin()
+        checkDiagonalWin()
     }, [board])
     return(
         <div>
